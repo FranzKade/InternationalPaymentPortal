821a4d4277321b004db995b54c80829d1c13f483
Tity Junior Tshabalala <titytshabalala@gmail.com>
2026-04-27 16:57:22 +0200

    Upload files for the front end

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{padding: "20px"}}>
+      <h1>Secure Customer International Payments Portal</h1>
+      <p>Frontend is working.</p>
+    </div>
+  );
+}
+
+export default App;
